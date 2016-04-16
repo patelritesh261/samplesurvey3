6d06ca51f35a94c40e7537c17bb52a60aca48320
patelritesh261 <patelritesh261@gmail.com>
2016-04-15 21:41:58 -0400

    url testing

--- server/routes/respond.ts
+++ server/routes/respond.ts
@@ -28,11 +28,11 @@ router.get('/presend', requireAuth, (req: express.Request, res: express.Response
       var ss=req.query;
     var sendgrid = require("sendgrid")("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");
 var email = new sendgrid.Email();
-
+var domain="http://samplesurvey3.azurewebsites.net/respond";
 email.addTo(ss.fromadd);
 email.setFrom("patelritesh261@gmail.com");
 email.setSubject("Plese give your important feedbak.");
-email.setHtml("<html><body><main><h2>HI There,</main><h4>Please do following survey and give your feedback</h4><table><tr><td>Survey Name : </td><td>"+ss.surveyName+"</td></tr><tr><td>Created By : </td><td>"+ss.displayName+"</td></tr><tr><td colspan=2><a href="+"http://samplesurvey3.azurewebsites.net/respond/"+ss.displayName+"/"+ss.surveyType+"/"+ss.surveyName+" >Give feedback</a></td></tr></table></body></html>");
+email.setHtml("<html><body><main><h2>HI There,</main><h4>Please do following survey and give your feedback</h4><table><tr><td>Survey Name : </td><td>"+ss.surveyName+"</td></tr><tr><td>Created By : </td><td>"+ss.displayName+"</td></tr><tr><td colspan=2><a href="+domain+"/"+ss.displayName+"/"+ss.surveyType+"/"+ss.surveyName+" >Give feedback</a></td></tr></table></body></html>");
 //email.setHtml("/respond/"+ss.displayName+"/"+ss.surveyType+"/"+ss.surveyName);
 
 
